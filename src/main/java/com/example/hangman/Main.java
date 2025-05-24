c117e4f56542a3e82d19c62e76332affc32c3ea7
neva4434 <aysenevaagca@gmail.com>
2025-05-25 01:09:53 +0300

    First Commit

---
+++ src/main/java/com/example/hangman/Main.java
@@ -0,0 +1,21 @@
+package com.example.hangman;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/hangman/login-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Hangman Game");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
